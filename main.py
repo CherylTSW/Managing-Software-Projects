0e51d02803509c082c8767bae6bc705d55b3d513
Kha-Hau <91935541+Kha-Hau@users.noreply.github.com>
2021-10-31 14:18:03 +0800

    Adjust GUI code to fit different screen size

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
-
+from lib import authentication as auth
+from lib import database as db
 from lib import inventory
 from tkinter import *
 from tkinter import messagebox
 from tkinter.font import BOLD
 
+auth_config = {
+            'host': "localhost",
+            'user': "root",
+            'password': "",
+            'database': "Authentication"
+        }
+auth_db_conn = db.create_db_connection(auth_config)
+# Add an account 
+auth.add_account(auth_db_conn, "placeholder", "123", "pol", "tato", 1)
+# Login
+print(auth.login(auth_db_conn, "placeholder", "123"))
+# Change password
+auth.password_change(auth_db_conn, auth.get_userID(auth_db_conn, "placeholder"), "456")
+# Delete an account with userID
+auth.delete_account(auth_db_conn, auth.get_userID(auth_db_conn, "placeholder"))
 
 '''
     Inventory Module
